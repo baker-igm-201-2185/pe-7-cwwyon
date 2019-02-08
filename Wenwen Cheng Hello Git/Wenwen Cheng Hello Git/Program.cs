11b5259933c8e5999203e5f5b3f0f2af8bb971c7
LydiaWu <lxw7227@g,rit.edu>
2019-02-08 12:08:57 +0800

    hello world

---
+++ Wenwen Cheng Hello Git/Wenwen Cheng Hello Git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wenwen_Cheng_Hello_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
